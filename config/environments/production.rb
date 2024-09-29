a613f7b1971e3e91add583dd52cf2f5bccdf9237
Brian Dear <superacidjax@me.com>
2024-09-29 17:59:12 +0200

    streamcamp --> AstraStream

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "streamcamp_production"
+  # config.active_job.queue_name_prefix = "astra_stream_production"
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
